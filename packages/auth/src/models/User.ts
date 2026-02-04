ac0c9453ae7e06ed957ecd7f2da08baeb06e6c19
gabriel-mend <crowofcode@gmail.com>
2026-02-03 22:54:50 -0300

    refactor: adicionando Models e condicionais nos subjects

--- packages/auth/src/models/User.ts
+++ packages/auth/src/models/User.ts
@@ -1,5 +1,9 @@
-import type { Role } from '../roles';
+import z from 'zod';
+import { roleSchema } from '../roles';
 
-export type User = {
-  role: Role;
-};
+export const userSchema = z.object({
+  id: z.string(),
+  role: roleSchema,
+});
+
+export type User = z.infer<typeof userSchema>;
